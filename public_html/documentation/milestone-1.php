558b26ec02ae13609903e9e8053acc829b1a9674
Michael Lee <mlee89@cnm.edu>
2017-10-18 22:32:47 -0600

    Finished milestone 1

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,11 +1,46 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Michael
- * Date: 10/13/2017
- * Time: 10:55 AM
- */
-
-?>
-
-Hello
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<title>A Cooking Journey</title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<h1>Cooking: A Cross-Cultural Joureny of Self-Discovery</h1>
+		<h2>Purpose</h2>
+		<p>My webstie will serve as way for me to share my love of cooking with others and to present a portfolio
+			of the recipes I've created while cooking.</p>
+		<h2>Audience</h2>
+		<p>The audience is fellow home chefs who seek ideas and inspiration pertaining to cooking</p>
+		<h2>Goal</h2>
+		<p>To build community around cooking- to increase the number of passionate home cooks in the United States. </p>
+		<h2>Persona</h2>
+		<h3>Name</h3>
+		<p>Jeff Hernandez</p>
+		<h3>Age</h3>
+		<p>25</p>
+		<h3>Profession</h3>
+		<p>High school chemistry teacher</p>
+		<h3>Technology</h3>
+		<p>iphone 6, dell pc</p>
+		<h3>Attitudes and Behaviors</h3>
+		<p>Attitudes and Behaivors: Jeff already knows about the basics of cooking, but wants to learn more.  He is studious
+			by nature, so he enjoys not only glancing at cookbooks for ideas, but also learning insights into cooking.  He will
+			spend time reading entire articles about various types of cuisines, such as Persian, or categories of food, such
+			as pastas. </p>
+		<h3>Frustrations and Needs</h3>
+		<p>Jeff is trying to build a more muscular physique, so he’s paying much attention to the healthfulness and nutrition
+			of the foods he eats.  He would like to gain 10 pounds of muscle by January.  Jeff works full time, so on weeknights,
+			he doesn’t have much time to look up recipes.  He would like to be able to look at a website for at most five minutes
+			before deciding on a recipe.  Also, Jeff has a few potential friends who cook, so he’s trying to learn more about the
+			hobby so that he can fit in more with those individuals.  On weekends, he has more time to read articles and try
+			more elaborate recipes.</p>
+		<h3>Goals</h3>
+		<p>Short on time during the week, jeff needs to be able to quickly identify quick, but tasty recipes to make for
+			him and his boyfriend, Mark.  Jeff is also a budding home cook and would like to learn more about what it means to enjoy
+			cooking more fully: the creativity involved, hosting a dinner party, trying new cuisines, telling one’s story through
+			cooking.</p>
+		<h3>User Story</h3>
+		<p>I need to be able to quickly pick a tasty, healthy, quick recipe for a weeknight dinner and learn more about
+		cooking.</p>
+	</body>
+</html>
